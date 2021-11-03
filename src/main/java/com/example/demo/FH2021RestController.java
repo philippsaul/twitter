1f396d36d781793a2f9f515614f71c9d86efb262
Philipp Saul <saul-p@t-online.de>
2021-11-03 14:48:10 +0100

    get and post methods

---
+++ src/main/java/com/example/demo/FH2021RestController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class FH2021RestController {
+    // @RequestMapping(path="/hallo", method = RequestMethod.GET)
+    @GetMapping("/hallo")
+    public String hallo(){
+        return "Hallo";
+    }
+
+    @GetMapping("/hallo/query")
+    public String halloQuery(@RequestParam(required = false, defaultValue = "Manfred") String name){
+        return "<h1>Hallo "+name+"</h1>";
+    }
+
+    @GetMapping("/hallo/path/{name}")
+    public String halloQueryPath(@PathVariable String name){
+        return "<h1>Hallo "+name+"</h1>";
+    }
+
+    @PostMapping("/hallo/body")
+    public String postTest(@RequestBody String name){
+        return "<h1>Hallo "+name+"</h1>";
+    }
+
+    @PostMapping("/hallo/dto")
+    public String postTest2(@RequestBody Who who){
+        return "<h1>Hallo "+who.getName()+"</h1>";
+    }
+}
