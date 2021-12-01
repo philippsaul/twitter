688d7aca87d4d9a688b167a41bb3ce30a7a4dfc9
Linus Etemi <drlinux12@gmail.com>
2021-12-01 15:55:20 +0100

    mwaf 1.12

--- src/main/java/com/example/demo/service/SimpleService.java
+++ src/main/java/com/example/demo/service/SimpleService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Person;
+
 public interface SimpleService {
     void createAndSaveGreeting(String name);
 }
--- src/main/java/com/example/demo/service/impl/SimpleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SimpleServiceImpl.java
@@ -1,27 +1,37 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.Person;
 import com.example.demo.repository.GreetingRepository;
+import com.example.demo.repository.PersonRepository;
 import com.example.demo.service.SimpleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.Greeting;
-import com.example.demo.service.SimpleService;
 
 
 @Service
 public class SimpleServiceImpl implements SimpleService {
     private final GreetingRepository greetingRepository;
+    private final PersonRepository personRepository;
 
     @Autowired
-    public SimpleServiceImpl(GreetingRepository greetingRepository) {
+    public SimpleServiceImpl(GreetingRepository greetingRepository, PersonRepository personRepository) {
         this.greetingRepository = greetingRepository;
+        this.personRepository = personRepository;
     }
 
     @Override
-    public void createAndSaveGreeting(String name) {
-/**        Greeting greeting = new Greeting();
-        greeting.setName(name);
-        greeting.setPhrase("hallo"+ name);
-        greetingRepository.save(greeting);**/
+    public void createAndSaveGreeting(String name){
+        Person person = personRepository.findByName(name);
+        if(person == null) {
+            Person newPerson = new Person();
+            newPerson.setName(name);
+            person = personRepository.save(newPerson);
+
+        }
+        Greeting greeting = new Greeting();
+        greeting.setPerson(person);
+        greeting.setPhrase("Hallo         " + name);
+        greetingRepository.save(greeting);
     }
 }
